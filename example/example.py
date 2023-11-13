9491d0502dee329de571509019f0453abd90a776
witchofizalith <85367796+witchofizalith@users.noreply.github.com>
2023-11-13 16:32:23 -0500

    wht
    why

--- example/example.py
+++ example/example.py
@@ -7,16 +7,16 @@ Created on Wed Apr 14 19:22:04 2021
 
 from medpc2excel.medpc_read import medpc_read
 
-f = 'xxx'
+f = 'C:/Users/Gabrielle/Documents/GitHub/medpc2excel/example/Magazine_wCameras.MPC'
 
 # medpc_read function return two outpus.
 # the first one is actual data
 
 # if you don't want export you data into *.xlsx file
-data, _ = medpc_read(f,save=False)  
+data, f = medpc_read(f,save=False)  
 
 # if you do want a *.xlsx file (default is save)
-data, _ = medpc_read(f)  # note that data is a dict-liked data structure
+data, f = medpc_read(f)  # note that data is a dict-liked data structure
 
 # to access the specific animal data in a specific date in the exmaple
 df = data['date']['id'] # df is a pandas dataframe for this animal in this date
